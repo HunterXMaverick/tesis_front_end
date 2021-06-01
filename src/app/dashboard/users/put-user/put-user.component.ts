09e45fd56a704d535b8b7570c63270c15c5afebd
vimhash <perlazajohao@gmail.com>
2021-05-31 19:34:46 -0500

    added user profile picture

--- src/app/dashboard/users/put-user/put-user.component.ts
+++ src/app/dashboard/users/put-user/put-user.component.ts
@@ -1,33 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { PersonService } from "../../../services/person.service";
-import { Person } from "../../../models/person";
 import Swal from "sweetalert2";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FilesService } from "src/app/services/files.service";
 
 @Component({
   selector: "app-put-user",
   templateUrl: "./put-user.component.html",
   styleUrls: ["./put-user.component.scss"],
 })
-export class PutUserComponent implements OnInit {
+export class PutUserComponent {
+  user: FormGroup;
   idUser: string;
   dataUser: any = [];
-  person: Person = {
-    names: "",
-    last_names: "",
-    phone: "",
-    title : ""
-  };
   logUser: any = [];
+  profile_picture_url: string = "";
 
   constructor(
     private router: ActivatedRoute,
     private personService: PersonService,
-    private routerLink: Router
-  ) {}
-
-  ngOnInit() {
-    this.getUserByEmail();
+    private routerLink: Router,
+    private filesService: FilesService,
+    public fb: FormBuilder
+  ) {
+    this.user = this.fb.group({
+      names: ["", [Validators.required]],
+      last_names: ["", [Validators.required]],
+      phone: ["", [Validators.required]],
+      profile_picture: [null],
+    });
     this.idUser = this.router.snapshot.params.id;
     this.getUserById();
   }
@@ -35,9 +37,26 @@ export class PutUserComponent implements OnInit {
   getUserById() {
     return this.personService.getUserById(this.idUser).subscribe((res: any) => {
       this.dataUser = res.data;
-      this.person.names = this.dataUser.names;
-      this.person.last_names = this.dataUser.last_names;
-      this.person.phone = this.dataUser.phone;
+      this.logUser = res.data;
+
+      if (this.dataUser.profile_picture) {
+        this.profile_picture_url = `http://localhost:3500/api/file/${this.dataUser.profile_picture}`;
+        console.log(this.profile_picture_url);
+
+        this.user.setValue({
+          names: this.dataUser.names,
+          last_names: this.dataUser.last_names,
+          phone: this.dataUser.phone,
+          profile_picture: this.dataUser.profile_picture,
+        });
+      } else {
+        this.user.setValue({
+          names: this.dataUser.names,
+          last_names: this.dataUser.last_names,
+          phone: this.dataUser.phone,
+          profile_picture: null,
+        });
+      }
     });
   }
 
@@ -51,19 +70,19 @@ export class PutUserComponent implements OnInit {
 
   putUser() {
     if (this.logUser.rol == "Asistente") {
-      this.person.phone = "0999999999";
+      this.user.setValue({
+        phone: "0999999999",
+      });
     }
-    if (this.person.names && this.person.last_names && this.person.phone) {
-      let dataPerson = {
-        person: this.person,
-      };
+    if (this.user.valid) {
       let pathOnlyLetters = /^[ñA-ZñÑáéíóúÁÉÍÓÚa-z _]*$/;
       let pathPhone = /^0[0-9]{1}[0-9]{8}$/;
-      let validateNames = pathOnlyLetters.test(dataPerson.person.names);
+      let validateNames = pathOnlyLetters.test(this.user.get("names").value);
       let validateLastNames = pathOnlyLetters.test(
-        dataPerson.person.last_names
+        this.user.get("last_names").value
       );
-      let validatePhone = pathPhone.test(dataPerson.person.phone);
+      let validatePhone = pathPhone.test(this.user.get("phone").value);
+
       if (validateNames && validateLastNames) {
         if (validatePhone) {
           Swal.fire({
@@ -75,21 +94,75 @@ export class PutUserComponent implements OnInit {
             confirmButtonText: "Confirmar",
           }).then((result) => {
             if (result.isConfirmed) {
-              this.personService.putPerson(this.idUser, dataPerson).subscribe(
-                (res) => {
-                  this.routerLink.navigate(["/dashboard/congresses"]);
-                },
-                (err) => {
-                  console.error(err);
-                }
-              );
-              Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Actualización exitosa",
-                showConfirmButton: false,
-                timer: 1500,
-              });
+              if (this.profile_picture_url == "") {
+                const formData: any = new FormData();
+                formData.append("file", this.user.get("profile_picture").value);
+
+                this.filesService
+                  .uploadFile("images", formData)
+                  .then((response) => {
+                    if (response.ok) {
+                      this.user.patchValue({
+                        profile_picture: `${response.data.directory}/${response.data.name}`,
+                      });
+                    } else {
+                      this.filesService
+                        .deleteFile(
+                          `${response.data.directory}`,
+                          `${response.data.name}`
+                        )
+                        .then((response: any) => {
+                          console.log(response.info);
+                        })
+                        .catch((error) => {
+                          console.error(error);
+                        });
+                    }
+                  })
+                  .then(() => {
+                    let dataPerson = {
+                      person: this.user.value,
+                    };
+
+                    this.personService
+                      .putPerson(this.idUser, dataPerson)
+                      .subscribe(
+                        (res) => {
+                          this.routerLink.navigate(["/dashboard/congresses"]);
+                        },
+                        (err) => {
+                          console.error(err);
+                        }
+                      );
+                    Swal.fire({
+                      position: "top-end",
+                      icon: "success",
+                      title: "Actualización exitosa",
+                      showConfirmButton: false,
+                      timer: 1500,
+                    });
+                  });
+              } else {
+                let dataPerson = {
+                  person: this.user.value,
+                };
+
+                this.personService.putPerson(this.idUser, dataPerson).subscribe(
+                  (res) => {
+                    this.routerLink.navigate(["/dashboard/congresses"]);
+                  },
+                  (err) => {
+                    console.error(err);
+                  }
+                );
+                Swal.fire({
+                  position: "top-end",
+                  icon: "success",
+                  title: "Actualización exitosa",
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+              }
             }
           });
         } else {
@@ -119,5 +192,14 @@ export class PutUserComponent implements OnInit {
         timer: 1500,
       });
     }
+  }
+
+  handleFileInput(event: any) {
+    const file = (event.target as HTMLInputElement).files[0];
+
+    this.user.patchValue({
+      profile_picture: file,
+    });
+    this.user.get("profile_picture").updateValueAndValidity();
   }
 }
