96976c3cf3bb39c1dc862623a38b7d1e8dd109bf
vimhash <perlazajohao@gmail.com>
2021-06-25 12:13:14 -0500

    fixed styles

--- src/app/dashboard/congresses/congresses.component.ts
+++ src/app/dashboard/congresses/congresses.component.ts
@@ -7,55 +7,57 @@ import { PersonService } from "../../services/person.service";
   templateUrl: "./congresses.component.html",
   styleUrls: ["./congresses.component.scss"],
 })
-export class CongressesComponent implements OnInit {
+export class CongressesComponent {
   persons: any = [];
   congress: any = [];
-  dataUser: any = [];
-  dataOrganizer: any = [];
+  dataUser: any;
+  // dataOrganizer: any = [];
 
   constructor(
-    private congressService: CongressService,
-    private personService: PersonService
-  ) {}
-
-  ngOnInit() {
+    private congressService: CongressService // private personService: PersonService
+  ) {
+    this.dataUser = JSON.parse(sessionStorage.getItem("_user-data"));
     this.getCongress();
-    this.getPersonByEmail();
-    this.getOrganizer();
   }
+
+  // ngOnInit() {
+  // this.getPersonByEmail();
+  // this.getOrganizer();
+  // }
 
   getCongress() {
     return this.congressService.getCongress().subscribe(
       (res) => {
         this.congress = res;
+        console.log(res);
       },
       (err) => console.error(err)
     );
   }
 
-  getPersonByEmail() {
-    return this.personService
-      .getUserByEmail(this.personService.email)
-      .subscribe(
-        (res: any) => {
-          this.dataUser = res.data;
-        },
-        (err) => console.error(err)
-      );
-  }
+  // getPersonByEmail() {
+  //   return this.personService
+  //     .getUserByEmail(this.personService.email)
+  //     .subscribe(
+  //       (res: any) => {
+  //         this.dataUser = res.data;
+  //       },
+  //       (err) => console.error(err)
+  //     );
+  // }
 
-  getOrganizer() {
-    return this.personService.getUsers().subscribe(
-      (res: any) => {
-        this.persons = res.data;
+  // getOrganizer() {
+  //   return this.personService.getUsers().subscribe(
+  //     (res: any) => {
+  //       this.persons = res.data;
 
-        this.persons.forEach((element) => {
-          if (element.rol == "Organizador") {
-            this.dataOrganizer = element;
-          }
-        });
-      },
-      (err) => console.error(err)
-    );
-  }
+  //       this.persons.forEach((element) => {
+  //         if (element.rol == "Organizador") {
+  //           this.dataOrganizer = element;
+  //         }
+  //       });
+  //     },
+  //     (err) => console.error(err)
+  //   );
+  // }
 }
--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,29 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { PersonService } from "../../services/person.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
   styleUrls: ["./sidebar.component.scss"],
 })
-export class SidebarComponent implements OnInit {
-  email: string;
-  dataUser: any = [];
+export class SidebarComponent {
+  dataUser: any;
 
-  constructor(private personService: PersonService) {
-    this.email = personService.email;
-  }
-
-  ngOnInit() {
-    this.getPersonByEmail();
-  }
-
-  getPersonByEmail() {
-    return this.personService.getUserByEmail(this.email).subscribe(
-      (res: any) => {
-        this.dataUser = res.data;
-      },
-      (err) => console.error(err)
-    );
+  constructor() {
+    this.dataUser = JSON.parse(sessionStorage.getItem("_user-data"));
   }
 }
