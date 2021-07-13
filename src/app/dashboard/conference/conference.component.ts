e18647df58f4185695e5434f43efcf5f594b3553
JohaoDev <perlazajohao@gmail.com>
2021-07-12 22:42:27 -0500

    added conference logic

--- src/app/dashboard/conference/conference.component.ts
+++ src/app/dashboard/conference/conference.component.ts
@@ -2,29 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { PostulationService } from '../../services/postulation.service';
 import { ConferenceService } from '../../services/conference.service';
 
-
 @Component({
   selector: 'app-conference',
   templateUrl: './conference.component.html',
-  styleUrls: ['./conference.component.scss']
+  styleUrls: ['./conference.component.scss'],
 })
 export class ConferenceComponent implements OnInit {
-  postulations: any = [];
-  conference: any = [];
+  dataUser: any = [];
+  postulationData: any = [];
+  conferences: any = [];
 
   constructor(
     private postulationService: PostulationService,
-    private conferenceService: ConferenceService) {
+    private conferenceService: ConferenceService
+  ) {
+    this.dataUser = JSON.parse(sessionStorage.getItem('_user-data')!);
+    this.getConferences();
   }
 
   ngOnInit(): void {
+    this.handleModal(false);
   }
 
-  getPostulation() {
-    return this.postulations.getPostulations().subscribe(
-      (res: any) => {
-        this.postulations = res.data;
+  getPostulation(postulation_id: string) {
+    return this.postulationService.getPostulations().subscribe((res: any) => {
+      res.data.forEach((element: any) => {
+        if (element._id == postulation_id) {
+          this.postulationData = element;
+        }
       });
+    });
   }
 
+  getConferences() {
+    return this.conferenceService.getConference().subscribe((res: any) => {
+      res.data.forEach((element: any) => {
+        if (element.reviewer_id === this.dataUser['_id']) {
+          this.conferences.push(element);
+        } else {
+          this.conferences = null;
+        }
+      });
+    });
+  }
+
+  handleModal(showModal: boolean) {
+    let modal: any = document.getElementById('modal');
+
+    if (showModal) {
+      modal.classList.remove('hidden');
+    } else {
+      modal.classList.add('hidden');
+    }
+  }
 }
