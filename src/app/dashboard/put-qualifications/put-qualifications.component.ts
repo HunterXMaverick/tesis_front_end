cb996b23d22011096e70c3d0ab21148b34c9b033
R0ger130296 <grarogccee@gmail.com>
2021-07-12 20:15:10 -0500

    add-componet-put-qualification

---
+++ src/app/dashboard/put-qualifications/put-qualifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-put-qualifications',
+  templateUrl: './put-qualifications.component.html',
+  styleUrls: ['./put-qualifications.component.scss']
+})
+export class PutQualificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
