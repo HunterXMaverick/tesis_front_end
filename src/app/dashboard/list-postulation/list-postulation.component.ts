581ca93a26adfa09e0fd1d369add5ee7b5855409
HunterXMaverick <jfo.silva98@gmail.com>
2021-07-14 16:24:42 -0500

    revision de listado de postualcion

--- src/app/dashboard/list-postulation/list-postulation.component.ts
+++ src/app/dashboard/list-postulation/list-postulation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostulationService } from '../../services/postulation.service';
+import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-list-postulation',
@@ -6,10 +8,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-postulation.component.scss']
 })
 export class ListPostulationComponent implements OnInit {
+  dataUser: any = [];
+  postulationData: any = [];
+  postulations: any = [];
 
-  constructor() { }
+  userById: any = [];
+  personId: any;
+  projectsSpeaker: any = [];
+  userData: any = '';
 
-  ngOnInit(): void {
+  constructor(
+    private personService: PersonService,
+    private postulationService: PostulationService
+  ) {
+    this.dataUser = JSON.parse(sessionStorage.getItem('_user-data')!);
   }
 
+  ngOnInit(): void {
+
+  }
+
+  // getPostulation(postulation_id: string) {
+  //   return this.postulationService.getPostulations().subscribe((res: any) => {
+  //     res.data.forEach((element: any) => {
+  //       if (element._id == postulation_id) {
+  //         this.postulationData = element;
+  //       }
+  //     });
+  //   });
+  // }
+
+  getPostulation() {
+    return this.postulationService.getPostulations().subscribe(
+      (res: any) => {
+        this.postulations = res.data;
+        this.postulations.postulations.forEach((element: any) => {
+          this.getUserById(element.person_id)
+          if(element.person_id == this.dataUser.person_id){
+            this.projectsSpeaker.push(element)
+          }
+        });
+      }
+    )
+  }
+
+
+  getUserById(id: string) {
+    return this.personService.getUserById(id).subscribe(
+      (res: any) => {
+        this.userById.push(res.data);
+        this.userData = res.data;
+      },
+      (err) => console.error(err)
+    );
+  }
 }
