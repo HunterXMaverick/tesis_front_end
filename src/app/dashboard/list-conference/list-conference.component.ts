17ed8fb4c4abba4fb0b12db7e7705539825fd69a
JohaoDev <perlazajohao@gmail.com>
2021-07-14 16:14:36 -0500

    fixed logic in list-conference component

--- src/app/dashboard/list-conference/list-conference.component.ts
+++ src/app/dashboard/list-conference/list-conference.component.ts
@@ -1,35 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { PostulationService } from '../../services/postulation.service';
 import { ConferenceService } from '../../services/conference.service';
+import { PersonService } from 'src/app/services/person.service';
 // import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-list-conference',
   templateUrl: './list-conference.component.html',
-  styleUrls: ['./list-conference.component.scss']
+  styleUrls: ['./list-conference.component.scss'],
 })
 export class ListConferenceComponent implements OnInit {
+  profile_picture_url: string = '';
   dataUser: any = [];
   postulationData: any = [];
   postulations: any = [];
   conferences: any = [];
   // userById: any = [];
   // personId: any;
-  // userData: any = '';
+  userData: any = '';
   // profile_picture_url: string = '';
-  
 
   constructor(
     private postulationService: PostulationService,
     private conferenceService: ConferenceService,
-    // private personService: PersonService
+    private personService: PersonService
   ) {
     this.dataUser = JSON.parse(sessionStorage.getItem('_user-data')!);
     this.getConferences();
   }
 
   ngOnInit(): void {
-    this.handleModal(false);   
+    this.handleModal(false);
+    this.handleModalSpeaker(false);
   }
 
   getPostulation(postulation_id: string) {
@@ -44,13 +46,8 @@ export class ListConferenceComponent implements OnInit {
 
   getConferences() {
     return this.conferenceService.getConference().subscribe((res: any) => {
-      res.data.forEach((element: any) => {
-        if (element.reviewer_id === this.dataUser['_id']) {
-          this.conferences.push(element);
-        } else {
-          this.conferences = null;
-        }
-      });
+      this.conferences = res.data;
+      console.log(this.conferences);
     });
   }
 
@@ -64,16 +61,23 @@ export class ListConferenceComponent implements OnInit {
     }
   }
 
-  // getUserById(id: string) {
-  //   return this.personService.getUserById(id).subscribe(
-  //     res => {
-  //       this.userById.push(res)
-  //     },
-  //     err => console.error(err)
-  //   )
-  // }
+  handleModalSpeaker(showModal: boolean) {
+    let modal: any = document.getElementById('modal-speaker');
 
-  // getUserId(id: string) {
-  //   this.personId = id;
-  // }
+    if (showModal) {
+      modal.classList.remove('hidden');
+    } else {
+      modal.classList.add('hidden');
+    }
+  }
+
+  getUserById(id: string) {
+    return this.personService.getUserById(id).subscribe(
+      (res: any) => {
+        this.userData = res.data;
+        this.profile_picture_url = `http://localhost:3500/api/file/${res.data.profile_picture}`;
+      },
+      (err) => console.error(err)
+    );
+  }
 }
