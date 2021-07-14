b4876ffd678847bacd1d876cc9f24dc1090a32d4
HunterXMaverick <jfo.silva98@gmail.com>
2021-07-13 23:57:46 -0500

    desarrollo de listado de conferencia

--- src/app/dashboard/list-conference/list-conference.component.ts
+++ src/app/dashboard/list-conference/list-conference.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostulationService } from '../../services/postulation.service';
+import { ConferenceService } from '../../services/conference.service';
+// import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-list-conference',
@@ -6,10 +9,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-conference.component.scss']
 })
 export class ListConferenceComponent implements OnInit {
+  dataUser: any = [];
+  postulationData: any = [];
+  postulations: any = [];
+  conferences: any = [];
+  // userById: any = [];
+  // personId: any;
+  // userData: any = '';
+  // profile_picture_url: string = '';
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private postulationService: PostulationService,
+    private conferenceService: ConferenceService,
+    // private personService: PersonService
+  ) {
+    this.dataUser = JSON.parse(sessionStorage.getItem('_user-data')!);
+    this.getConferences();
   }
 
+  ngOnInit(): void {
+    this.handleModal(false);   
+  }
+
+  getPostulation(postulation_id: string) {
+    return this.postulationService.getPostulations().subscribe((res: any) => {
+      res.data.forEach((element: any) => {
+        if (element._id == postulation_id) {
+          this.postulationData = element;
+        }
+      });
+    });
+  }
+
+  getConferences() {
+    return this.conferenceService.getConference().subscribe((res: any) => {
+      res.data.forEach((element: any) => {
+        if (element.reviewer_id === this.dataUser['_id']) {
+          this.conferences.push(element);
+        } else {
+          this.conferences = null;
+        }
+      });
+    });
+  }
+
+  handleModal(showModal: boolean) {
+    let modal: any = document.getElementById('modal');
+
+    if (showModal) {
+      modal.classList.remove('hidden');
+    } else {
+      modal.classList.add('hidden');
+    }
+  }
+
+  // getUserById(id: string) {
+  //   return this.personService.getUserById(id).subscribe(
+  //     res => {
+  //       this.userById.push(res)
+  //     },
+  //     err => console.error(err)
+  //   )
+  // }
+
+  // getUserId(id: string) {
+  //   this.personId = id;
+  // }
 }
