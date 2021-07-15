fcab96e021c6cf4f02cccc4303446fe91dc75a50
JohaoDev <perlazajohao@gmail.com>
2021-07-14 19:34:06 -0500

    list-postulation

--- src/app/dashboard/list-postulation/list-postulation.component.ts
+++ src/app/dashboard/list-postulation/list-postulation.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { PostulationService } from 'src/app/services/postulation.service';
 
 @Component({
   selector: 'app-list-postulation',
   templateUrl: './list-postulation.component.html',
-  styleUrls: ['./list-postulation.component.scss']
+  styleUrls: ['./list-postulation.component.scss'],
 })
 export class ListPostulationComponent implements OnInit {
+  postulations: any = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private postulationService: PostulationService) {
+    this.getPostulations();
   }
 
+  ngOnInit(): void {}
+
+  getPostulations() {
+    return this.postulationService.getPostulations().subscribe(
+      (res: any) => {
+        res.data.forEach((element: any) => {
+          if (element.status == 'Aprobado') {
+            this.postulations.push(element);
+            // this.getUserById(element.person_id);
+            // if (element.person_id == this.dataUser._id) {
+            //   this.projectsSpeaker.push(element);
+            // }
+          }
+        });
+      },
+      (err) => console.error(err)
+    );
+  }
 }
