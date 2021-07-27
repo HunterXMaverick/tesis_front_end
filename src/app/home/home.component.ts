bbb45d90a85d92f9a7c0ab22e6a9dec62ec46493
R0ger130296 <grarogccee@gmail.com>
2021-07-26 21:06:25 -0500

    get congress home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CongressService } from '../services/congress.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  congress_habilitado: any;
+  congress_pendientes: any= [];
+  congress: any;
+  see_logo: string = '';
+
+  constructor(
+    private congressService: CongressService,
+  ){
+  }
   ngOnInit() {
     this.handleModal(false);
+    this.getCongress(); 
+  }
+
+  getCongress() {
+    return this.congressService.getCongress().subscribe(
+      (res: any) => {
+        if (res.data.length == 0) {
+          this.congress = null;
+        } else {
+          res.data.forEach((element: any) => {
+            if (
+              element.status_congress == 'Habilitado'
+            ) {
+              this.congress_habilitado = element;
+              this.see_logo = `http://localhost:3500/api/file/${element.logo}`;
+            }
+            if(
+              element.status_congress == 'Pendiente'
+            ){
+              this.congress_pendientes.push(element);
+            }
+          });
+        }
+      },
+      (err) => console.error(err)
+    );
   }
 
   handleModal(showModal: boolean) {
