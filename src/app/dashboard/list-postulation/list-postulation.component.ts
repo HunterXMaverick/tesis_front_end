c3314255b3b51077c33d30c497b57dfd00e740d1
unknown <ble.ona@yavirac.edu.ec>
2021-08-06 21:06:00 -0500

    fix information postulationTs, modal with info

--- src/app/dashboard/list-postulation/list-postulation.component.ts
+++ src/app/dashboard/list-postulation/list-postulation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostulationService } from '../../services/postulation.service';
-
+import { PostulationPasticipantsService } from '../../services/postulationPasticipants.service';
 @Component({
   selector: 'app-list-postulation',
   templateUrl: './list-postulation.component.html',
@@ -9,27 +9,53 @@ import { PostulationService } from '../../services/postulation.service';
 export class ListPostulationComponent implements OnInit {
   postulations: any = [];
   congressSelected: any;
+  dataUser: any = [];
+  postulationData: any = [];
 
-  constructor(private postulationService: PostulationService) {
+  constructor(private postulationParticipantsService: PostulationPasticipantsService,  private postulationService: PostulationService,) {
     this.congressSelected = sessionStorage.getItem('activeCongress');
+    this.dataUser = JSON.parse(sessionStorage.getItem('_user-data')!);
     this.getPostulations();
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.handleModal(false);
+  }
+  handleModal(showModal: boolean) {
+    let modal: any = document.getElementById('modal');
 
+    if (showModal) {
+      modal.classList.remove('hidden');
+    } else {
+      modal.classList.add('hidden');
+    }
+  }
+  getPostulation(postulation_id: string) {
+    return this.postulationService.getPostulations().subscribe((res: any) => {
+      res.data.forEach((element: any) => {
+        if (element._id == postulation_id) {
+          this.postulationData = element;
+        }
+      });
+    });
+  }
   getPostulations() {
-    return this.postulationService.getPostulations().subscribe(
+    return this.postulationParticipantsService.getPostulationParticipantsController().subscribe(
       (res: any) => {
         res.data.forEach((element: any) => {
+        
           if (
+            this.dataUser._id == element.person_id && 
             element.status == 'Aprobado' &&
             element.congress_id == this.congressSelected
           ) {
             this.postulations.push(element);
+            console.log(element);
           }
         });
       },
-      (err) => console.error(err)
+      (err:any) => console.error(err)
     );
   }
 }
